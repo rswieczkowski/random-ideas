cdd4fe7cbb1aeb5f8a7355f30b585ca0cb6a40ad
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-12-11 11:51:57 +0100

    Compleated REST API

--- server.js
+++ server.js
@@ -1,51 +1,21 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+connectDB();
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Some random text',
-    tag: 'Technology',
-    username: 'Tony Stark',
-    date: '2023-05-12',
-  },
-  {
-    id: 2,
-    text: 'Some random text 2',
-    tag: 'Invention',
-    username: 'Steven Rogers',
-    date: '2023-05-16',
-  },
-  {
-    id: 3,
-    text: 'Some random text 3',
-    tag: 'Software',
-    username: 'Bruce Banner',
-    date: '2023-05-16',
-  },
-];
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to RandomIdeas API' });
 });
 
-// Get all ideas
-app.get('/api/ideas', (req, res) => {
-  res.json({ success: true, data: ideas });
-});
-
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: 'Resource nof found' });
-  }
-  res.json({ success: true, data: idea });
-});
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
